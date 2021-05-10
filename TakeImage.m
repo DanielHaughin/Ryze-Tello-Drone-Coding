f3ac65f6b5e609783ac3dc49fca6d1af7931a6ad
DanielHaughin <60963580+DanielHaughin@users.noreply.github.com>
2021-05-10 17:07:48 +0100

    Add files via upload

--- TakeImage.m
+++ TakeImage.m
@@ -7,13 +7,12 @@ format compact
 myDrone = ryze();
 %Connects to camera
 myCam = camera(myDrone);
-droneFig = figure();
-ax = axes(droneFig);
-frame = snapshot(myCam);
-im = image(ax, zeros(size(frame),'uint8'));
-droneFig = preview(myCam,im);
-movegui(droneFig,[800 300]);
 %Take-off drone
 takeoff(myDrone);
+%display live feed
+preview(myCam);
+%Take and display image
+frame = snapshot(myCam);
+imshow(frame);
 %Land drone
 land(myDrone);
