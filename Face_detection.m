65be2fe11e596b3416d7306164221fb77161f336
DanielHaughin <60963580+DanielHaughin@users.noreply.github.com>
2021-05-10 13:23:36 +0100

    Add files via upload
    Face detection now highlights features of a face to assist with tracking

--- Face_detection.m
+++ Face_detection.m
@@ -6,10 +6,16 @@ format compact
 % Create a cascade detector object.
 faceDetector = vision.CascadeObjectDetector();
 
-% Read a video frame and run the face detector.
+% Read an image and run the face detector.
 img = imread('face.jfif');
 bbox = step(faceDetector, img);
 
 % Draw the returned bounding box around the detected face.
 img = insertShape(img, 'Rectangle', bbox);
-figure; imshow(img); title('Detected face');
+
+%detect features to identify face
+points = detectMinEigenFeatures(rgb2gray(img), 'ROI', bbox);
+
+% Display the detected points.
+figure; imshow(img); hold on; title('Detected features');
+plot(points);
